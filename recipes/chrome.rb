c67b53000d9008117bc38d7295a288d1eb15cf97
Dmytro Makhno <dmakhno@griddynamics.com>
2012-09-18 13:27:09 +0300

    more flexible configuration using json, relations fixes

--- recipes/chrome.rb
+++ recipes/chrome.rb
@@ -1,3 +1,5 @@
+include_recipe 'selenium::default'
+	
 case node['kernel']['machine']
 when 'i686'
   arch = 'linux32'
@@ -8,7 +10,7 @@ end
 package 'chromium-browser'
 
 remote_file "/usr/src/chromedriver_#{arch}_#{node['selenium']['chromedriver_version']}.zip" do
-  source "http://code.google.com/p/chromedriver/downloads/detail?name=chromedriver_#{arch}_#{node['selenium']['chromedriver_version']}.zip"
+  source "http://chromedriver.googlecode.com/files/chromedriver_#{arch}_#{node['selenium']['chromedriver_version']}.zip"
   action :create_if_missing
   notifies :run, "execute[unpack chromedriver]", :immediately
 end
@@ -20,6 +22,6 @@ end
 
 file '/usr/local/bin/chromedriver' do
   mode 0755
-  owner default['selenium']['user']
-  group default['selenium']['user']
+  owner node['selenium']['user']
+  group node['selenium']['user']
 end
